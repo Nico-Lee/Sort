31bcbc5b2f4e031820f52db1fd74efb431f5c91f
进击的二哈 <helloleex@outlook.com>
2016-07-09 22:52:07 +0800

    Update SelectSort.h

--- SelectSort.h
+++ SelectSort.h
@@ -1,3 +1,12 @@
+#pragma once
+#include<iostream>
+#include<assert.h>
+#include"windows.h"
+#include"time.h"
+using namespace std;
+//选择排序和堆排序都属于选择排序
+
+
 //选择排序
 void SelectSort(int* arry, int size)
 {
@@ -17,7 +26,21 @@ void SelectSort(int* arry, int size)
 		flag++;
 	}
 }
-//****************************************************
+
+
+void testSelectSort()
+{
+	int arry[] = { 1, 9, 2, 3, 8, 5, 6, 15, 6, 7 };
+	int size = sizeof(arry) / sizeof(int);
+	cout << size << endl;
+	SelectSort(arry, size);
+	for (int i = 0; i < size; i++)
+	{
+		cout << arry[i] << "  ";
+	}
+}
+
+
 //堆排序
 void AdjustDown(int* a, size_t size, size_t parent)//下调函数
 {
@@ -51,4 +74,53 @@ void HeapSort(int* a, size_t size)
 		swap(a[0], a[size - 1 - i]);
 		AdjustDown(a, size - i - 1, 0);
 	}
+}
+
+
+void FindMaxK(int N, int K)//找出N个数据中最大的K个数据
+{
+	int* arryN = new int[N];
+	int* maxK = new int[K];
+	for (int i = 0; i < N; ++i)
+	{
+		arryN[i] = (rand() % 100);
+	}
+	for (int i = 0; i < N; ++i)
+	{
+		cout << arryN[i] << " ";
+	}
+	cout << endl;
+	for (int i = 0; i < K; i++)
+	{
+		maxK[i] = arryN[i];
+	}
+	for (int i = K; i < N; ++i)
+	{
+		HeapSort(maxK, K);
+		maxK[0] = arryN[i];
+	}
+	for (int i = 0; i < K; ++i)
+	{
+		cout << maxK[i] << " ";
+	}
+}
+void testHeapSort()
+{
+	//int arry[] = { 1, 9, 2, 3, 8, 5, 6, 15, 6, 7, 0, -1000 };
+	//int size = sizeof(arry) / sizeof(int);
+	//HeapSort(arry, size);
+	//for (int i = 0; i < size; i++)
+	//{
+	//	cout << arry[i] << "  ";
+	//}
+	int N = 10;
+	int K = 5;
+	FindMaxK(N, K);
+}
+
+
+int main()
+{
+	testSelectSort();
+	return 0;
 }
